c08d41253c6057d1d3bd3cde5b215a6363a8e21a
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-01-23 10:25:39 +0500

    исправил баг с выходом из приложения

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,28 @@
-import Header from '../header/header.js'
-import CompaniesPage from '../companiesPage/companies.js'
-import {getAppTemplate} from "./template.js";
+import Header from "../header/header.js";
+import CompaniesPage from "../companiesPage/companies.js";
+import { getAppTemplate } from "./template.js";
 
-class App{
-    constructor(appName) {
-        this.appName = appName
-    }
+class App {
+  constructor(appName) {
+    this.appName = appName;
+  }
 
-    render = () => {
-        document.querySelector('body').innerHTML = getAppTemplate()
-        let headerConatainer = document.querySelector('.header__container')
-        let pageContainer = document.querySelector('.page__container')
-        new Header(headerConatainer, this.appName).init()
-        new CompaniesPage(pageContainer).init()
-    }
+  render = () => {
+    document.querySelector("body").innerHTML = getAppTemplate();
+    let headerConatainer = document.querySelector(".header__container");
+    let pageContainer = document.querySelector(".page__container");
+    new Header(headerConatainer, this.appName).init();
+    new CompaniesPage(pageContainer).init();
+  };
 }
-let app = new App("Companies Searcher")
-app.render()
+let app = new App("Companies Searcher");
+app.render();
 
+if (!localStorage.getItem("user"))
+  window.location.pathname = "src/templates/login.html";
 
-if(!localStorage.getItem('user'))
-  window.location.pathname = "../../templates/login.html"
-
-let exit = document.querySelector('#exit')
-exit.addEventListener('click', () =>{
-  localStorage.removeItem('user')
-  window.location.pathname = "../../templates/login.html"
-})
+let exit = document.querySelector("#exit");
+exit.addEventListener("click", () => {
+  localStorage.removeItem("user");
+  window.location.pathname = "src/templates/login.html";
+});
